b169419ba34b4881fc5a26afa2ca75e77525f911
wastl (Sebastian Meyer) <seb.meyer@fau.de>
2018-03-13 08:49:26 +0000

    argument matching
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/confIntIndependentAUCDiff.R
+++ pkg/biostatUZH/R/confIntIndependentAUCDiff.R
@@ -4,8 +4,9 @@
 ## controls is a n x 2 matrix
 ## each row corresponds to measurements from one case/control
 
-confIntIndependentAUCDiff <- function(casesA, controlsA, casesB, controlsB, type="Wald", conf.level = 0.95){
-
+confIntIndependentAUCDiff <- function(casesA, controlsA, casesB, controlsB,
+                                      type="Wald", conf.level = 0.95)
+{
     ncontrolsA <- nrow(controlsA)
     ncasesA <- nrow(casesA)
     ncontrolsB <- nrow(controlsB)
@@ -15,6 +16,8 @@ confIntIndependentAUCDiff <- function(casesA, controlsA, casesB, controlsB, type
     resA <- confIntAUC(casesA, controlsA, conf.level = conf.level)
     resB <- confIntAUC(casesB, controlsB, conf.level = conf.level)
     factor <- qnorm((1 + conf.level) / 2)
+
+    type <- match.arg(type, c("Wald", "Logit"))
 
     if(type=="Wald"){
         ## take intervals on original scale
@@ -31,17 +34,16 @@ confIntIndependentAUCDiff <- function(casesA, controlsA, casesB, controlsB, type
         se.D <- sqrt(seA^2+seB^2)
         D.lower <- D - factor * se.D
         D.upper <- D + factor * se.D
-        
+
         res <- data.frame(matrix(NA, ncol = 4))
         colnames(res) <- c("outcome", "lower", "estimate", "upper")
-        res[1, 2:4] <- resA[1,2:4] # Wald interval on original scale 
-        res[2, 2:4] <- resB[1,2:4] # Wald interval on original scale 
+        res[1, 2:4] <- resA[1,2:4] # Wald interval on original scale
+        res[2, 2:4] <- resB[1,2:4] # Wald interval on original scale
         res[3, 2:4] <- c(D.lower, D, D.upper)
         res[, 1] <- c("AUC Test 1", "AUC Test 2", "AUC Difference")
-
     }
+
     if(type=="Logit"){
-        
         ## take intervals on logit scale
         aucA <- resA[2,3]
         aucB <- resB[2,3]
@@ -50,17 +52,18 @@ confIntIndependentAUCDiff <- function(casesA, controlsA, casesB, controlsB, type
         lowerB <- resB[2,2]
         upperA <- resA[2,4]
         upperB <- resB[2,4]
-        
+
         ## Apply Newcombe trick
         D.lower <- D - sqrt((aucA - lowerA) ^ 2 + (aucB - upperB) ^ 2)
         D.upper <- D + sqrt((aucA - upperA) ^ 2 + (aucB - lowerB) ^ 2)
 
         res <- data.frame(matrix(NA, ncol = 4))
         colnames(res) <- c("outcome", "lower", "estimate", "upper")
-        res[1, 2:4] <- resA[2,2:4] # Wald interval on logit scale 
-        res[2, 2:4] <- resB[2,2:4] # Wald interval on logit scale 
+        res[1, 2:4] <- resA[2,2:4] # Wald interval on logit scale
+        res[2, 2:4] <- resB[2,2:4] # Wald interval on logit scale
         res[3, 2:4] <- c(D.lower, D, D.upper)
         res[, 1] <- c("AUC Test 1", "AUC Test 2", "AUC Difference")
     }
+
     return(res)
 }
