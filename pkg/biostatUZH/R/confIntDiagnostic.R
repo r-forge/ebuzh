88bac914d3d5876255e6c70a8b674fb3c02d593e
lheld (Leonhard Held) <leonhard.held@uzh.ch>
2018-08-15 16:06:50 +0000

    PPV and NPV added in confIntDiagnostic
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/confIntDiagnostic.R
+++ pkg/biostatUZH/R/confIntDiagnostic.R
@@ -1,19 +1,45 @@
-confIntDiagnostic <- function(tp, fp, tn, fn, conf.level = 0.95)
+confIntDiagnostic <- function(tp, fp, tn, fn, conf.level = 0.95, pv=FALSE, prev=NA)
 {
     stopifnot(is.wholenumber(tp), is.wholenumber(fp),
               is.wholenumber(tn), is.wholenumber(fn),  conf.level<1,
               conf.level>0)
+    if(pv==FALSE)
+        res <- data.frame(matrix(NA, ncol = 4))
+    if(pv==TRUE)
+        res <- data.frame(matrix(NA, ncol = 6))
 
-    res <- data.frame(matrix(NA, ncol = 4))
     colnames(res) <- c("type", "lower", "estimate", "upper")
 
     res[1, 2:4] <- wilson(x=tp, n=tp+fn, conf.level = conf.level)
     res[2, 2:4] <- wilson(x=tn, n=tn+fp, conf.level = conf.level)
-    res[3, 2:4] <- confIntRiskRatio(x=c(tp,fp), n=c(tp+fn, fp+tn), conf.level = conf.level)
-    res[4, 2:4] <- confIntRiskRatio(x=c(fn,tn), n=c(tp+fn, tn+fp), conf.level = conf.level)
+    LRplus <- confIntRiskRatio(x=c(tp,fp), n=c(tp+fn, fp+tn), conf.level = conf.level)
+    LRminus <- confIntRiskRatio(x=c(fn,tn), n=c(tp+fn, tn+fp), conf.level = conf.level)
+    res[3, 2:4] <- LRplus
+    res[4, 2:4] <- LRminus
 
-    res[, 1] <- c("Sensitivity", "Specificity", "LRplus", "LRminus")
+    if(pv==TRUE){
+        if(is.na(prev)){
+            res[5, 2:4] <- wilson(x=tp, n=tp+fp, conf.level = conf.level)
+            res[6, 2:4] <- wilson(x=tn, n=tn+fn, conf.level = conf.level)
+        }
+        if(!is.na(prev)){
+            stopifnot(prev>0, prev<1)
+            prev.odds <- prev/(1-prev)
+            PPV.odds <- prev.odds*LRplus
+            PPV <- PPV.odds/(1+PPV.odds)
+            NPV.inv.odds <- prev.odds*LRminus
+            NPV <- rev(1/(1+NPV.inv.odds))
+            res[5, 2:4] <- PPV
+            res[6, 2:4] <- NPV
+        }
+        
+    }
+    
+    if(pv==FALSE)
+        res[, 1] <- c("Sensitivity", "Specificity", "LRplus", "LRminus")
+    if(pv==TRUE)
+        res[, 1] <- c("Sensitivity", "Specificity", "LRplus", "LRminus", "PPV", "NPV")
     res <- res[,c(1,3,2,4)]
     return(res)
-
+    
 }
