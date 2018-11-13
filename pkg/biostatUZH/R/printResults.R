c104140ae016fb3a4ebfb0473210e314ffb5de28
lheld (Leonhard Held) <leonhard.held@uzh.ch>
2018-11-13 13:14:23 +0000

    minor change in printResults
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/printResults.R
+++ pkg/biostatUZH/R/printResults.R
@@ -1,9 +1,10 @@
 printResults <- function(theta, se.theta, conf.level=0.95, digits=3){
   z <- qnorm((1+conf.level)/2)
-  ci <- theta + c(-1,1)*z*se.theta
+  ci.l <- theta - z*se.theta
+  ci.u <- theta + z*se.theta
   p <-  2*pnorm(abs(theta)/se.theta, lower.tail=FALSE)
   effect <- round(theta, digits=digits)
-  ci <- formatCI(ci, digits=digits, text="english")
+  ci <- formatCI(c(ci.l, ci.u), digits=digits, text="english")
   p <- formatPval(p)
   res <- c(effect, ci, p)
   names(res) <- list("Effect", 
