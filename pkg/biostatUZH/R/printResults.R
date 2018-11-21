a018765488bddf032b6d4ebc0e7496496dafd3b1
lheld (Leonhard Held) <leonhard.held@uzh.ch>
2018-11-21 13:28:54 +0000

    commit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@80 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/printResults.R
+++ pkg/biostatUZH/R/printResults.R
@@ -1,14 +1,14 @@
-printResults <- function(theta, se.theta, conf.level=0.95, digits=3){
-  z <- qnorm((1+conf.level)/2)
-  ci.l <- theta - z*se.theta
-  ci.u <- theta + z*se.theta
-  p <-  2*pnorm(abs(theta)/se.theta, lower.tail=FALSE)
-  effect <- round(theta, digits=digits)
-  ci <- formatCI(c(ci.l, ci.u), digits=digits, text="english")
-  p <- formatPval(p)
-  res <- c(effect, ci, p)
-  names(res) <- list("Effect", 
-                     sprintf('%d%% Confidence Interval', conf.level*100),
-                     "P-value")
-  print(t(res), quote=FALSE)
+printResults <- function(theta, se.theta, conf.level=0.95, FUN=identity, digits=3){
+    z <- qnorm((1+conf.level)/2)
+    ci.l <- theta - z*se.theta
+    ci.u <- theta + z*se.theta
+    p <-  2*pnorm(abs(theta)/se.theta, lower.tail=FALSE)
+    effect <- round(FUN(theta), digits=digits)
+    ci <- formatCI(c(FUN(ci.l), FUN(ci.u)), digits=digits, text="english")
+    p <- formatPval(p)
+    res <- c(effect, ci, p)
+    names(res) <- list("Effect", 
+                       sprintf('%d%% Confidence Interval', conf.level*100),
+                       "P-value")
+    print(t(res), quote=FALSE)
 }
