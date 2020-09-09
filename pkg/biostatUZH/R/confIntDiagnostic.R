a76e20bdb055e031086ad13fced1b62c26a03364
lheld (Leonhard Held) <leonhard.held@uzh.ch>
2020-09-09 06:54:42 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@91 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/confIntDiagnostic.R
+++ pkg/biostatUZH/R/confIntDiagnostic.R
@@ -3,7 +3,7 @@ confIntDiagnostic <- function(tp, fp, tn, fn, conf.level = 0.95, cohort=FALSE, p
     stopifnot(is.wholenumber(tp), is.wholenumber(fp),
               is.wholenumber(tn), is.wholenumber(fn),  conf.level<1,
               conf.level>0)
-    res <- data.frame(matrix(NA, nrow = 7, ncol = 4))
+    res <- data.frame(matrix(NA, nrow = 8, ncol = 4))
 
     colnames(res) <- c("type", "lower", "estimate", "upper")
 
@@ -12,8 +12,10 @@ confIntDiagnostic <- function(tp, fp, tn, fn, conf.level = 0.95, cohort=FALSE, p
     if((tp>0)&(fp>0)&(fn>0)&(tn>0)){
         LRplus <- confIntRiskRatio(x=c(tp,fp), n=c(tp+fn, fp+tn), conf.level = conf.level)
         LRminus <- confIntRiskRatio(x=c(fn,tn), n=c(tp+fn, tn+fp), conf.level = conf.level)
+        DOR <- confIntOddsRatio(x=c(tp,fp), n=c(tp+fn, tn+fp), conf.level = conf.level)
         res[3, 2:4] <- LRplus
         res[4, 2:4] <- LRminus
+        res[5, 2:4] <- DOR
     }
     
     if(!is.na(pr)){
@@ -23,17 +25,17 @@ confIntDiagnostic <- function(tp, fp, tn, fn, conf.level = 0.95, cohort=FALSE, p
         PPV <- PPV.odds/(1+PPV.odds)
         NPV.inv.odds <- pr.odds*LRminus
         NPV <- rev(1/(1+NPV.inv.odds))
-        res[5, 2:4] <- PPV
-        res[6, 2:4] <- NPV
-        res[7, 3] <- pr
+        res[6, 2:4] <- PPV
+        res[7, 2:4] <- NPV
+        res[8, 3] <- pr
     }
     if(is.na(pr) & (cohort==TRUE)){
-        res[5, 2:4] <- wilson(x=tp, n=tp+fp, conf.level = conf.level)
-        res[6, 2:4] <- wilson(x=tn, n=tn+fn, conf.level = conf.level)
-        res[7, 2:4] <- wilson(x=tp+fn, n=tp+tn+fp+fn, conf.level = conf.level)
+        res[6, 2:4] <- wilson(x=tp, n=tp+fp, conf.level = conf.level)
+        res[7, 2:4] <- wilson(x=tn, n=tn+fn, conf.level = conf.level)
+        res[8, 2:4] <- wilson(x=tp+fn, n=tp+tn+fp+fn, conf.level = conf.level)
     }
 
-    res[, 1] <- c("Sensitivity", "Specificity", "LRplus", "LRminus", "PPV", "NPV", "prevalence")
+    res[, 1] <- c("Sensitivity", "Specificity", "LRplus", "LRminus", "DOR", "PPV", "NPV", "Prevalence")
     res <- res[,c(1,3,2,4)]
     return(res)
     
